3138e41919662081469d48d63a2625af15b66f9a
James Hancock <jhancockdarwin@gmail.com>
2020-07-24 15:29:08 -0400

    Initial checking of web code

---
+++ example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,16 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:flutter_screen_recording_web/flutter_screen_recording_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  WebFlutterScreenRecording.registerWith(registry.registrarFor(WebFlutterScreenRecording));
+  registry.registerMessageHandler();
+}
