e3619ecc4d853e189cb771ad278ab1d360977a4b
Ismael Monje <imonje@isvisoft.com>
2020-09-25 15:08:19 +0200

    change foreground plugin

--- lib/flutter_screen_recording.dart
+++ lib/flutter_screen_recording.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:foreground_service/foreground_service.dart';
+import 'package:flutter_foreground_plugin/flutter_foreground_plugin.dart';
 
 class FlutterScreenRecording {
   static const MethodChannel _channel = const MethodChannel('flutter_screen_recording');
@@ -22,49 +22,31 @@ class FlutterScreenRecording {
   static Future<String> get stopRecordScreen async {
     final String path = await _channel.invokeMethod('stopRecordScreen');
     if (Platform.isAndroid) {
-      await ForegroundService.stopForegroundService();
+      await FlutterForegroundPlugin.stopForegroundService();
     }
     return path;
   }
 
-  static void _maybeStartFGS(String titleNotification, String messageNotification) async {
+  static  _maybeStartFGS(String titleNotification, String messageNotification) async {
     if (Platform.isAndroid) {
-      ///if the app was killed+relaunched, this function will be executed again
-      ///but if the foreground service stayed alive,
-      ///this does not need to be re-done
-      if (!(await ForegroundService.foregroundServiceIsStarted())) {
-        await ForegroundService.setServiceIntervalSeconds(5);
-
-        //necessity of editMode is dubious (see function comments)
-        await ForegroundService.notification.startEditMode();
-
-        await ForegroundService.notification.setTitle(titleNotification);
-        await ForegroundService.notification.setText(messageNotification);
-
-        await ForegroundService.notification.finishEditMode();
-
-        await ForegroundService.startForegroundService(_foregroundServiceFunction);
-        await ForegroundService.getWakeLock();
-      }
-
-      ///this exists solely in the main app/isolate,
-      ///so needs to be redone after every app kill+relaunch
-      await ForegroundService.setupIsolateCommunication((data) {
-        print("main received: $data");
-      });
+      await FlutterForegroundPlugin.setServiceMethodInterval(seconds: 5);
+      await FlutterForegroundPlugin.setServiceMethod(globalForegroundService);
+      return await FlutterForegroundPlugin.startForegroundService(
+        holdWakeLock: false,
+        onStarted: () async {
+          print("Foreground on Started");
+        },
+        onStopped: () {
+          print("Foreground on Stopped");
+        },
+        title: titleNotification,
+        content: messageNotification,
+        iconName: "org_thebus_foregroundserviceplugin_notificationicon",
+      );
     }
   }
 
-  static void _foregroundServiceFunction() {
-    print("The current time is: ${DateTime.now()}");
-    //ForegroundService.notification.setText("The time was: ${DateTime.now()}");
-
-    if (!ForegroundService.isIsolateCommunicationSetup) {
-      ForegroundService.setupIsolateCommunication((data) {
-        print("bg isolate received: $data");
-      });
-    }
-
-    ForegroundService.sendToPort("message from bg isolate");
+  static void globalForegroundService() {
+    print("current datetime is ${DateTime.now()}");
   }
 }
