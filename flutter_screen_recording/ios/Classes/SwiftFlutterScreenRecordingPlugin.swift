2d3182b99753a30df1dd07c686572ff9f24fa9b0
onstonboy <vanchuong119@gmail.com>
2024-09-04 19:35:27 +0800

    Optimize init source in background before recording

--- flutter_screen_recording/ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
+++ flutter_screen_recording/ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
@@ -60,146 +60,164 @@ public class SwiftFlutterScreenRecordingPlugin: NSObject, FlutterPlugin {
 
     //Use ReplayKit to record the screen
     //Create the file path to write to
-    let documentsPath =
-      NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString
-    self.videoOutputURL = URL(fileURLWithPath: documentsPath.appendingPathComponent(nameVideo))
+      DispatchQueue.global(qos: .userInitiated).async { [self] in
+        let documentsPath =
+          NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString
+        self.videoOutputURL = URL(fileURLWithPath: documentsPath.appendingPathComponent(self.nameVideo))
 
-    //Check the file does not already exist by deleting it if it does
-    do {
-      try FileManager.default.removeItem(at: videoOutputURL!)
-    } catch {}
+        //Check the file does not already exist by deleting it if it does
+        do {
+            try FileManager.default.removeItem(at: self.videoOutputURL!)
+        } catch {}
 
-    do {
-      try videoWriter = AVAssetWriter(outputURL: videoOutputURL!, fileType: AVFileType.mp4)
-    } catch let writerError as NSError {
-      print("Error opening video file", writerError)
-      videoWriter = nil
-      return
-    }
+        do {
+            try self.videoWriter = AVAssetWriter(outputURL: videoOutputURL!, fileType: AVFileType.mp4)
+        } catch let writerError as NSError {
+            print("Error opening video file", writerError)
+            videoWriter = nil
+            DispatchQueue.main.async {
+                self.myResult?(false)
+            }
+          return
+        }
 
-    //Create the video settings
-    if #available(iOS 11.0, *) {
+        //Create the video settings
+        if #available(iOS 11.0, *) {
 
-      var codec = AVVideoCodecJPEG
+            var codec = AVVideoCodecType.jpeg
+            var videoSettings: [String : Any]
 
-      if recordAudio {
-        codec = AVVideoCodecH264
-      }
+            if recordAudio {
+              codec = AVVideoCodecType.h264
+            }
 
-//      let videoSettings: [String: Any] = [
-//        AVVideoCodecKey: codec,
-//        AVVideoWidthKey: screenSize.width,
-//        AVVideoHeightKey: screenSize.height,
-//        AVVideoCompressionPropertiesKey: [
-//          AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel,
-//          AVVideoAverageBitRateKey: 6000000
-//        ],
-//      ]
+    //      let videoSettings: [String: Any] = [
+    //        AVVideoCodecKey: codec,
+    //        AVVideoWidthKey: screenSize.width,
+    //        AVVideoHeightKey: screenSize.height,
+    //        AVVideoCompressionPropertiesKey: [
+    //          AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel,
+    //          AVVideoAverageBitRateKey: 6000000
+    //        ],
+    //      ]
+            if self.recordAudio {
+                videoSettings = [
+                    AVVideoCodecKey  : codec,
+                    AVVideoWidthKey  : UIScreen.main.bounds.width,
+                    AVVideoHeightKey : UIScreen.main.bounds.height,
+                    AVVideoCompressionPropertiesKey: [
+                    AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel,
+                    AVVideoAverageBitRateKey: 6000000
+                   ],
+                ]
+            } else {
+                videoSettings = [
+                    AVVideoCodecKey  : codec,
+                    AVVideoWidthKey  : UIScreen.main.bounds.width,
+                    AVVideoHeightKey : UIScreen.main.bounds.height,
+                ]
+            }
 
-        let videoSettings: [String : Any] = [
-            AVVideoCodecKey  : AVVideoCodecH264,
-            AVVideoWidthKey  : UIScreen.main.bounds.width,
-            AVVideoHeightKey : UIScreen.main.bounds.height,
-            AVVideoCompressionPropertiesKey: [
-            AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel,
-            AVVideoAverageBitRateKey: 6000000
-           ],
-        ]
 
-      if recordAudio {
+            if self.recordAudio {
 
-        let audioOutputSettings: [String: Any] = [
-          AVNumberOfChannelsKey: 2,
-          AVFormatIDKey: kAudioFormatMPEG4AAC,
-          AVSampleRateKey: 44100,
-        ]
+                let audioOutputSettings: [String: Any] = [
+                  AVNumberOfChannelsKey: 2,
+                  AVFormatIDKey: kAudioFormatMPEG4AAC,
+                  AVSampleRateKey: 44100,
+                ]
 
-        audioInput = AVAssetWriterInput(
-          mediaType: AVMediaType.audio, outputSettings: audioOutputSettings)
-        videoWriter?.add(audioInput)
+                audioInput = AVAssetWriterInput(
+                  mediaType: AVMediaType.audio, outputSettings: audioOutputSettings)
+                videoWriter?.add(audioInput)
 
-      }
+            }
 
-      //Create the asset writer input object which is actually used to write out the video
-      videoWriterInput = AVAssetWriterInput(
-        mediaType: AVMediaType.video, outputSettings: videoSettings)
-      videoWriter?.add(videoWriterInput!)
+          //Create the asset writer input object which is actually used to write out the video
+          videoWriterInput = AVAssetWriterInput(
+            mediaType: AVMediaType.video, outputSettings: videoSettings)
+          videoWriter?.add(videoWriterInput!)
 
-    }
+        }
 
-    //Tell the screen recorder to start capturing and to call the handler
-    if #available(iOS 11.0, *) {
+        //Tell the screen recorder to start capturing and to call the handler
+        if #available(iOS 11.0, *) {
 
-      if recordAudio {
-        RPScreenRecorder.shared().isMicrophoneEnabled = true
-      } else {
-        RPScreenRecorder.shared().isMicrophoneEnabled = false
-
-      }
-
-      RPScreenRecorder.shared().startCapture(
-        handler: { (cmSampleBuffer, rpSampleType, error) in
-          guard error == nil else {
-            //Handle error
-            print("Error starting capture")
-            self.myResult!(false)
-            return
+          if recordAudio {
+            RPScreenRecorder.shared().isMicrophoneEnabled = true
+          } else {
+            RPScreenRecorder.shared().isMicrophoneEnabled = false
           }
 
-          if CMSampleBufferDataIsReady(cmSampleBuffer) {
-
-            DispatchQueue.main.async {
-              switch rpSampleType {
-              case RPSampleBufferType.video:
-                print("writing sample....")
-                if self.videoWriter?.status == AVAssetWriter.Status.unknown {
-
-                  if (self.videoWriter?.startWriting) != nil {
-                    print("Starting writing")
-                    self.myResult!(true)
-                    self.videoWriter?.startWriting()
-                    self.videoWriter?.startSession(
-                      atSourceTime: CMSampleBufferGetPresentationTimeStamp(cmSampleBuffer))
-                  }
-                }
-
-                if self.videoWriter?.status == AVAssetWriter.Status.writing {
-                  if self.videoWriterInput?.isReadyForMoreMediaData == true {
-                    print("Writing a sample")
-                    if self.videoWriterInput?.append(cmSampleBuffer) == false {
-                      print(" we have a problem writing video")
+          RPScreenRecorder.shared().startCapture(
+            handler: { (cmSampleBuffer, rpSampleType, error) in
+              guard error == nil else {
+                  //Handle error
+                  print("Error starting capture")
+                  DispatchQueue.main.async {
                       self.myResult!(false)
+                  }
+                  return
+              }
+
+              if CMSampleBufferDataIsReady(cmSampleBuffer) {
+
+                DispatchQueue.main.async {
+                  switch rpSampleType {
+                  case RPSampleBufferType.video:
+                    print("writing sample....")
+                    if self.videoWriter?.status == AVAssetWriter.Status.unknown {
+
+                      if (self.videoWriter?.startWriting) != nil {
+                        print("Starting writing")
+                        self.myResult!(true)
+                        self.videoWriter?.startWriting()
+                        self.videoWriter?.startSession(
+                          atSourceTime: CMSampleBufferGetPresentationTimeStamp(cmSampleBuffer))
+                      }
                     }
+
+                    if self.videoWriter?.status == AVAssetWriter.Status.writing {
+                      if self.videoWriterInput?.isReadyForMoreMediaData == true {
+                        print("Writing a sample")
+                        if self.videoWriterInput?.append(cmSampleBuffer) == false {
+                          print(" we have a problem writing video")
+                          self.myResult!(false)
+                        }
+                      }
+                    }
+
+                  case RPSampleBufferType.audioMic:
+                    if self.recordAudio {
+                      if self.audioInput.isReadyForMoreMediaData {
+                        // print("audioMic data added")
+                        if self.audioInput.append(cmSampleBuffer) == false {
+                          print(" we have a problem writing audio")
+                          self.myResult!(false)
+                        }
+                      }
+                    }
+
+                  default:
+                  ();
+                  // print("not a video sample, so ignore")
                   }
                 }
-
-              case RPSampleBufferType.audioMic:
-                if self.recordAudio {
-                  if self.audioInput.isReadyForMoreMediaData {
-                    // print("audioMic data added")
-                    if self.audioInput.append(cmSampleBuffer) == false {
-                      print(" we have a problem writing audio")
+              }
+            }) { (error) in
+              guard error == nil else {
+                //Handle error
+                  print("Screen record not allowed")
+                  DispatchQueue.main.async {
                       self.myResult!(false)
-                    }
                   }
-                }
 
-              default:
-              ();
-              // print("not a video sample, so ignore")
+                return
               }
             }
-          }
-        }) { (error) in
-          guard error == nil else {
-            //Handle error
-            print("Screen record not allowed")
-            self.myResult!(false)
-            return
-          }
+        } else {
+          //Fallback on earlier versions
         }
-    } else {
-      //Fallback on earlier versions
     }
   }
 
